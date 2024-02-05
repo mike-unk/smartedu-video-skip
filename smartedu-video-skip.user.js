61886944965ccbadad635f47512abe936308e017
mike-unk <53944618+mike-unk@users.noreply.github.com>
2024-02-05 12:15:44 +0800

    Update smartedu-video-skip.user.js

--- smartedu-video-skip.user.js
+++ smartedu-video-skip.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         智慧教育平台视频跳过
 // @namespace    https://github.com/mike-unk/smartedu-video-skip
-// @version      1.0.0
+// @version      1.0.1
 // @description  跳过国家中小学智慧教育平台的视频
 // @updateURL    https://github.com/mike-unk/smartedu-video-skip/raw/main/smartedu-video-skip.user.js
 // @downloadURL  https://github.com/mike-unk/smartedu-video-skip/raw/main/smartedu-video-skip.user.js
 // @author       mike-unk
 // @match        https://basic.smartedu.cn/*
 // @license      MIT
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=smartedu.cn
+// @icon         https://basic.smartedu.cn/favicon.ico
 // @grant        none
 // ==/UserScript==
 
